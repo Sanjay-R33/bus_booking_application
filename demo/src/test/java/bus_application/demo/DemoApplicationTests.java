11763f24e38bcc09f54d19ea32c7884f51448e9f
Sanjay-R33 <sanjayr200433@gmail.com>
2026-04-16 11:41:31 +0530

    initial commit

---
+++ demo/src/test/java/bus_application/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bus_application.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
